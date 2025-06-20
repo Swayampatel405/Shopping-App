bd5c53939fccbc10aa957020373300c0d0beceab
Swayampatel405 <swyampatel405@gmail.com>
2025-06-21 00:46:26 +0530

    Adding SplashScreen

--- app/src/main/java/com/appvantage/shoppingapp/MainActivity.kt
+++ app/src/main/java/com/appvantage/shoppingapp/MainActivity.kt
@@ -5,13 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
-import com.appvantage.shoppingapp.presentation.navigation.MyApp
-import com.appvantage.shoppingapp.presentation.screens.LoginScreenUi
+import com.appvantage.shoppingapp.presentation.screens.MainScreen
 import com.appvantage.shoppingapp.ui.theme.ShoppingAppTheme
 import com.google.firebase.auth.FirebaseAuth
 import dagger.hilt.android.AndroidEntryPoint
@@ -27,7 +21,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ShoppingAppTheme {
-                 MyApp(firebaseAuth = firebaseAuth)
+                MainScreen(firebaseAuth)
             }
         }
     }
---
+++ app/src/main/java/com/appvantage/shoppingapp/presentation/screens/MainScreen.kt
@@ -0,0 +1,80 @@
+package com.appvantage.shoppingapp.presentation.screens
+
+import android.app.Notification
+import android.os.Handler
+import android.os.Looper
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.appvantage.shoppingapp.R
+import com.google.firebase.auth.FirebaseAuth
+import com.appvantage.shoppingapp.presentation.navigation.MyApp
+
+@Composable
+fun MainScreen(
+    firebaseAuth: FirebaseAuth
+//    onPayTest:()->Unit
+) {
+    val showSplash = remember { mutableStateOf(true) }
+    LaunchedEffect(Unit) {
+        Handler(Looper.getMainLooper()).postDelayed({ showSplash.value = false }, 3000)
+    }
+
+    if (showSplash.value) {
+        SplashScreen()
+    } else {
+        MyApp(firebaseAuth)
+    }
+
+}
+
+@Composable
+fun SplashScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize().background(color = Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Image(
+                painter = painterResource(R.drawable.ic_logo),
+                contentDescription = "App Icon",
+                modifier = Modifier.size(300.dp)
+            )
+            Spacer(modifier = Modifier.height(5.dp))
+            BasicText(
+                text = "Welcome to the clothing store",
+                style = MaterialTheme.typography.labelLarge.copy(
+                    color = Color.White,
+                    fontSize = 18.sp,
+                    textAlign = TextAlign.Center
+                )
+            )
+
+        }
+    }
+}
